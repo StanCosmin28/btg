73c25d96b96f19139e51c266a74d7bd99ac4fdbc
Stan Cosmin <stancosminioan@gmail.com>
2025-05-08 12:17:47 +0300

    fix footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
             <div className="flex flex-col text-left gap-4">
-              <div className="flex flex-row gap-4 justify-evenly">
+              <div className="flex flex-row md:flex-col gap-4 justify-evenly">
                 <a href="/" className="flex items-center group">
                   <img
                     src="public/btg-logo.svg"
@@ -22,7 +22,7 @@ export default function Footer() {
                 </p>
               </div>
               <div>
-                <button className="transition-all duration-300 hover:scale-102 max-w-4xl w-full p-2 sm:p-3 text-base sm:text-lg md:text-xl text-white font-bold hover:bg-blue-800 hover:text-white  cursor-pointer bg-[#0655D1]">
+                <button className="transition-all duration-300 hover:scale-110 hover:rounded-xl max-w-4xl w-full p-2 sm:p-3 text-base sm:text-lg md:text-xl text-white font-bold hover:bg-blue-800 hover:text-white  cursor-pointer bg-[#0655D1]">
                   Contactează-ne
                 </button>
               </div>
@@ -111,7 +111,7 @@ export default function Footer() {
               <h2 className="mb-6 text-sm font-semibold text-white uppercase">
                 Contact
               </h2>
-              <ul className="text-gray-400 font-medium">
+              {/* <ul className="text-gray-400 font-medium">
                 <li className="">
                   <a
                     href="#"
@@ -136,7 +136,84 @@ export default function Footer() {
                     tel
                   </a>
                 </li>
-              </ul>
+              </ul> */}
+              <div className="flex justify-left flex-col gap-2 min-w-[250px]">
+                {/* Email Component */}
+                {/* <div className="group flex flex-col items-center text-center w-20 sm:w-44 md:w-48 p-4 transition-transform hover:-translate-y-1"> */}
+                <a
+                  href="mailto:tehnic@btgromania.ro"
+                  className="group flex flex-row items-center justify-between gap-2 text-center w-20 sm:w-44 md:w-48 transition-transform hover:-translate-y-1"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 text-white transition-colors group-hover:text-blue-200 hidden md:block"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="1.5"
+                  >
+                    <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
+                    <polyline points="22,6 12,13 2,6" />
+                  </svg>
+                  <p
+                    href="mailto:tehnic@btgromania.ro"
+                    className="text-white text-sm sm:text-base font-medium hover:underline transition-colors group-hover:text-blue-200"
+                  >
+                    tehnic@btgromania.ro
+                  </p>
+                </a>
+                {/* </div> */}
+
+                {/* Location Component */}
+                <a
+                  href="/BTG-location"
+                  className="group flex flex-row items-center justify-between gap-2 text-center sm:w-44 md:w-48  transition-transform hover:-translate-y-1"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 text-white transition-colors group-hover:text-blue-200 hidden md:block"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="1.5"
+                  >
+                    <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0z" />
+                    <circle cx="12" cy="10" r="3" />
+                  </svg>
+                  <p
+                    href="https://maps.app.goo.gl/your-location"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-left text-white text-sm sm:text-base font-medium hover:underline transition-colors group-hover:text-blue-200"
+                  >
+                    {/* Iasi, Aleea Rozelor, Nr. 6A */}
+                    Iasi, Aleea Rozelor, Nr. 6A, Demisol
+                  </p>
+                </a>
+
+                {/* Phone Component */}
+                <a
+                  href="BTG-mobile"
+                  className="group flex flex-row items-center justify-start gap-2 text-center w-20 sm:w-44 md:w-48 transition-transform hover:-translate-y-1"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5 text-white transition-colors group-hover:text-blue-200 hidden md:block"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="1.5"
+                  >
+                    <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
+                  </svg>
+                  <p
+                    href="tel:+400232250885"
+                    className="text-left text-white text-sm sm:text-base font-medium hover:underline transition-colors group-hover:text-blue-200"
+                  >
+                    0232.250.885
+                  </p>
+                </a>
+              </div>
             </div>
           </div>
         </div>
